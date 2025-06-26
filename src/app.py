5a3cce984823907d22dd97033a2d4c65bb577961
Matthew Spah <spahmatthew@gmail.com>
2025-06-25 18:46:40 -0700

    feat: add APA_ACCESS_TOKEN env support

--- src/app.py
+++ src/app.py
@@ -1,8 +1,10 @@
 import os
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 app.config["APA_REFRESH_TOKEN"] = os.getenv("APA_REFRESH_TOKEN")
+app.config["APA_ACCESS_TOKEN"] = os.getenv("APA_ACCESS_TOKEN")
 
 
 @app.route("/")
